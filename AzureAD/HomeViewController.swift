a938ab3d8741a4c1f870fa540f6d5fd9e1f365c0
Subodh Jena <jenasubodh@gmail.com>
2017-05-10 12:08:34 +0530

    Basic Project Structure

---
+++ AzureAD/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  AzureAD
+//
+//  Created by Subodh Jena on 5/10/17.
+//  Copyright © 2017 Subodh Jena. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
