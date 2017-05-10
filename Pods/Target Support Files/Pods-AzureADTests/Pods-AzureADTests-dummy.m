af27c4554094134b0ab20ea7a558140df5ccd215
Subodh Jena <jenasubodh@gmail.com>
2017-05-10 12:03:40 +0530

    POD Initiated

---
+++ Pods/Target Support Files/Pods-AzureADTests/Pods-AzureADTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AzureADTests : NSObject
+@end
+@implementation PodsDummy_Pods_AzureADTests
+@end
---
+++ Pods/Target Support Files/Pods-AzureADUITests/Pods-AzureADUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AzureADUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AzureADUITests
+@end
